b34e7219a1a3789221c2a5a4b3786ef2accf3674
mshubat <mshubat2@uwo.ca>
2019-09-04 08:57:51 -0400

    merge sort added

--- Python/Sorting Algorithms/MergeSort.py
+++ Python/Sorting Algorithms/MergeSort.py
@@ -11,3 +11,51 @@ Time Complexity: O(n*logn)
 Space Complexity: O(n) [Depends]
 '''
 
+
+def mergeSort(arr, low, high):
+
+    if high > low:
+        former = mergeSort(arr[0:len(arr)//2], 1, len(arr)//2)
+        ladder = mergeSort(arr[len(arr)//2:len(arr)], 1, len(arr)-len(arr)//2)
+        return merge(former, ladder)
+    else:
+        return arr
+
+
+def merge(former, ladder):
+    combined = []
+    former_empty = False
+    ladder_empty = False
+    fi = 0
+    li = 0
+
+    while (not former_empty and not ladder_empty):
+        if former[fi] < ladder[li]:
+            combined.append(former[fi])
+            fi += 1
+            if fi == len(former):
+                former_empty = True
+        else:
+            combined.append(ladder[li])
+            li += 1
+            if li == len(ladder):
+                ladder_empty = True
+
+    if former_empty:
+        while li < len(ladder):
+            combined.append(ladder[li])
+            li += 1
+    elif ladder_empty:
+        while fi < len(former):
+            combined.append(former[fi])
+            fi += 1
+
+    return combined
+
+
+if __name__ == "__main__":
+    items = [3,6,4,7,5,1,2,8,10,9]
+    print(items)
+
+    items = mergeSort(items, 1, len(items))
+    print(items)
